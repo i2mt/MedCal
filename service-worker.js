417f98e0d0dd4ec8eacb08be2937408d27869d3b
i2mt <mohammad.mahdi.ta@gmail.com>
2026-06-03 15:41:37 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,51 +1,15 @@
-// Service Worker for MedCalc Pro PWA
-const CACHE_NAME = 'medcalc-pro-v2.0.6';
-const urlsToCache = [
-    './',
-    './index.html',
-    './style.css',
-    './script.js',
-    './converters.js',
-    './drugDatabase.js',
-    './manifest.json',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-    'https://cdn.fontcdn.ir/Font/Persian/Mitra/Mitra.css'
-];
-
-// Install event
+// Service Worker – NO CACHING, always fresh from network
 self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then(cache => {
-                return cache.addAll(urlsToCache);
-            })
-    );
+    console.log('SW installed – skipping cache');
+    self.skipWaiting();
 });
-
-// Activate event
 self.addEventListener('activate', event => {
+    console.log('SW activated – deleting all caches');
     event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (cacheName !== CACHE_NAME) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
+        caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
     );
+    return self.clients.claim();
 });
-
-// Fetch event
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            })
-    );
+    event.respondWith(fetch(event.request));
 });
