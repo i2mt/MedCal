0020f857659724141d899e3f821245937b86782d
i2mt <mohammad.mahdi.ta@gmail.com>
2026-06-07 15:25:10 +0330

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Always tries to fetch the latest version.
 // Falls back to cache only when offline.
 
-const CACHE_NAME = 'medcalc-pro-v2.2.0';
+const CACHE_NAME = 'medcalc-pro-v3.0.0';
 
 const urlsToCache = [
     './',
